5ce6c673a31a2678811290a182df5f4772f70646
Nayab Shaik <nayab.shaik@insightglobal.com>
2026-07-07 15:10:46 +0530

    iOS

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import platform
-if platform.system() == "Mac":
+if platform.system() == "iOS":
     print("Linux Distribution:", platform.freedesktop_os_release()["PRETTY_NAME"])
 else:
     print("Not running Linux")
