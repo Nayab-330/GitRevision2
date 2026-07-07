76f5dc3e6dc35219cd8fc56f3948d654062715ad
Nayab Rasool Shaik <79514277+Nayab-330@users.noreply.github.com>
2026-07-07 15:09:07 +0530

    Update app.py
    updated as Windows

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import platform
-if platform.system() == "Mac":
+if platform.system() == "Windows":
     print("Linux Distribution:", platform.freedesktop_os_release()["PRETTY_NAME"])
 else:
     print("Not running Linux")
